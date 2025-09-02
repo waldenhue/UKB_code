b2641502a9773f281e56eec205e8da373354981c
waldenhue <huk180308@gmail.com>
2025-09-02 16:24:01 +0800

    submit

---
+++ association_tests/gwas/clumping.sh
@@ -0,0 +1,11 @@
+bfile_path="/mnt/project/merged_data"
+clump_input="/opt/notebooks/tg/gwas.txt"
+
+plink \
+	--bfile $bfile_path \
+	--clump $clump_input \
+	--clump-p1 1.96e-8 \
+	--clump-p2 1e-3 \
+	--clump-r2 0.1 \
+	--clump-kb 250 \
+	--out "tg.clumped"
