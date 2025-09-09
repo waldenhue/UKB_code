41dbe665e51e5ad568168b7c169a7a049df14fe6
waldenhue <huk180308@gmail.com>
2025-09-09 10:50:56 +0800

    update

--- integration_comparison/late_integration.py
+++ integration_comparison/late_integration.py
@@ -17,17 +17,18 @@ def read_info(pheno_symbol,rv_type):
         pheno_name = 'triglycerides'
     else:
         pheno_name = pheno_symbol
-    file_path = '/data/med-hudh/taiyiv2/'+pheno_symbol+'/'
+    file_path = './'+pheno_symbol+'/'
     df_pheno = pd.read_parquet(file_path+'pheno.parquet',engine='pyarrow')
     df_cv = pd.read_parquet(file_path+'cv.parquet',engine='pyarrow')
     df_rv = pd.read_parquet(file_path+'rv.parquet',engine='pyarrow')
     cv_index = [i.split('_')[0] for i in df_cv.index.tolist()]
     df_cv.index = cv_index
-    df_cv['cvPRS'] = df_cv.sum(axis=1)
+    df_cv['cvPRS'] = df_cv.sum(axis=1) ## calculating common PRS 
     df_cv = pd.DataFrame(df_cv['cvPRS'])
-    df_rv['rvPRS'] = df_rv.sum(axis=1)
+    df_rv['rvPRS'] = df_rv.sum(axis=1) ## calculating rare PRS
     df_rv = pd.DataFrame(df_rv['rvPRS'])
     
+    ## extracing train and test ids both having pheno, cv and rv info
     with open(file_path+'test_id.txt','r') as file:
         test_ids = [line.strip() for line in file]
     with open(file_path+'tran1_id.txt','r') as file:
@@ -35,8 +36,7 @@ def read_info(pheno_symbol,rv_type):
     train_ids = list(set(df_pheno.index.tolist())&set(df_cv.index.tolist())&set(df_rv.index.tolist())&set(train_ids))
     test_ids = list(set(df_pheno.index.tolist())&set(df_cv.index.tolist())&set(df_rv.index.tolist())&set(test_ids))
     
-
-    
+    ## making train and test dataframes    
     df_pheno_train = df_pheno[df_pheno.index.isin(train_ids)]
     df_cv_train = df_cv[df_cv.index.isin(train_ids)]
     df_rv_train = df_rv[df_rv.index.isin(train_ids)]
@@ -45,14 +45,15 @@ def read_info(pheno_symbol,rv_type):
     df_cv_test = df_cv[df_cv.index.isin(test_ids)]
     df_rv_test = df_rv[df_rv.index.isin(test_ids)]
 
-    df_rv_train = df_rv_train
-    df_rv_test = df_rv_test
+    ## making traing and test dataframes according to variant type
     if rv_type == 'rv':
         df_train_final  = pd.concat([df_pheno_train[pheno_name],df_cv_train,df_rv_train],axis=1)
         df_test_final  = pd.concat([df_pheno_test[pheno_name],df_cv_test,df_rv_test],axis=1)
     else:
         df_train_final  = pd.concat([df_pheno_train[pheno_name],df_cv_train],axis=1)
         df_test_final  = pd.concat([df_pheno_test[pheno_name],df_cv_test,],axis=1)   
+
+    ## extracting features and labels
     feature_names = df_train_final.drop(pheno_name, axis=1).columns.tolist()    
     X_train = df_train_final.drop(pheno_name,axis=1).values
     y_train = df_train_final[pheno_name].values
@@ -98,10 +99,8 @@ def result_output(pheno_symbol,rv_type):
     X_list, y_list, feature_names = read_info(pheno_symbol, rv_type)
     X_train = X_list[0]
     X_test = X_list[1]
-    X_test_m1 = X_list[2]
     y_train = y_list[0]
     y_test = y_list[1]
-    y_test_m1 = y_list[2]
     
     
     # Pipeline of OLS
@@ -124,9 +123,7 @@ def result_output(pheno_symbol,rv_type):
     y_pred = linear_pipeline.predict(X_test)
     test_score = r2_score(y_test, y_pred)
     mse_all = mean_squared_error(y_test,y_pred)
-    y_pred_carriers = linear_pipeline.predict(X_test_m1)
-    test_score_carriers = r2_score(y_test_m1, y_pred_carriers)
-    mse_carriers = mean_squared_error(y_test_m1,y_pred_carriers)
+
     if pheno_symbol == 'aa':
         output_symbol = 'AA'
     elif pheno_symbol == 'igf1':
@@ -137,9 +134,8 @@ def result_output(pheno_symbol,rv_type):
         output_symbol = 'ST'
     else:
         output_symbol = 'HDL'
-    list1 = [output_symbol,'linear','cv+rv','all',test_score,mse_carriers]
-    list2 = [output_symbol,'linear','cv+rv','carriers',test_score_carriers,mse_all]
-    df_report = pd.DataFrame([list1,list2],columns=['Trait','Model','Variant','Group','R2','MSE'])
+    list1 = [output_symbol,'linear','cv+rv',test_score,mse_all]
+    df_report = pd.DataFrame([list1],columns=['Trait','Model','Variant','R2','MSE'])
     df_report.to_csv('/data/med-hudh/format_output/report/linear_'+pheno_symbol+'_'+rv_type+'.csv')
 
 for trait in ['st','igf1','aa','tg','hdl']:
